d41b38c38f456f63d813a9dfe4421b259523242c
LewisErick <lewis.erick@outlook.com>
2020-08-29 13:07:33 -0500

    Removed credentials

--- outgoing_call.py
+++ outgoing_call.py
@@ -1,9 +1,11 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
+import os
 from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
--- outgoing_sms.py
+++ outgoing_sms.py
@@ -1,15 +1,14 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
+import os
 from twilio.rest import Client
 
-
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Hi there!',
-                              from_='+15017122661',
-                              to='+15558675310'
+                              from_='+19142299068',
+                              to='+528110801708'
                           )
 
 print(message.sid)
