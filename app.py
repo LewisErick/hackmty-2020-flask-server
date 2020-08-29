0a1b49024c65ea8ab2db47e7208859861c87854b
LewisErick <lewis.erick@outlook.com>
2020-08-29 13:14:05 -0500

    Moved outgoing calls and SMS to Flask application

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
+from twilio.rest import Client
+
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
 
+# Set up Twilio client.
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+client = Client(account_sid, auth_token)
 
 @app.route("/answer", methods=['GET', 'POST'])
 def answer_call():
@@ -14,6 +21,28 @@ def answer_call():
     resp.say("Thank you for calling! Have a great day.", voice='alice')
 
     return str(resp)
+
+@app.route("/send-sms/<user_phone>", methods=['POST'])
+def send_message():
+    user_phone = user_phone
+    message = client.messages.create(
+                body='Hi there!',
+                from_=os.environ.get("TWILIO_NUMBER"),
+                to=str(user_phone)
+            )
+
+    return message.sid
+
+@app.route("/place-call/<user_phone>", methods=['POST'])
+def place_call():
+    user_phone = user_phone
+    call = client.calls.create(
+                url='http://demo.twilio.com/docs/voice.xml',
+                from_=os.environ.get("TWILIO_NUMBER"),
+                to=str(user_phone),
+            )
+
+    return call.sid
 
 if __name__ == "__main__":
     app.run(debug=True)
